37d767588a02eba4b85751b6e8042b956101a94e
Parth <parthrpandya98@gmail.com>
2017-12-02 10:38:11 +0530

    volume halved

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var audio = [new Audio("audio/Just The Way You Are.mp3"), new Audio("audio/Marvin Gaye.mp3"), new Audio("audio/see you again.mp3"), new Audio("audio/talking to the moon.mp3"), new Audio("audio/Vanilla Twilight.mp3"), new Audio("audio/Thinking Out Loud.mp3"), new Audio("audio/i wanna grow old with you.mp3"), new Audio("audio/when you say nothing at all.mp3")];
 var index = 0;
+
+
 function playNext(index){
+  audio[index].volume = 0.5;
   audio[index].play(); //this will play the element
   $(audio[index]).bind('ended', function(){ //this will bind a function to the "ended" event
     //increment the index to target the next element
